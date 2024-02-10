4b4c49b9e41097f0d315515795ece89719d4ee5e
GrzegorzSipika <sipika.grzegorz@gmail.com>
2024-02-10 23:38:44 +0100

    No i działa, w końcu

--- Odczytywanie rysunków z zielonych ramek - oddzielonych od siebie.py
+++ Odczytywanie rysunków z zielonych ramek - oddzielonych od siebie.py
@@ -2,131 +2,152 @@ import os
 import logging
 from datetime import datetime
 from PIL import Image
-
+import cv2
 import threading
 import sys
+import numpy as np
 
-def clean_frames(image):
-    grayscale_img = image.convert("L")
-    cleaned_img = grayscale_img.point(lambda p: 0 if p < 65 else 255)
-    cleaned_img = cleaned_img.convert("RGB")
-    logging.info(f"Oczyszczono klatkę")
-    return cleaned_img
+def setup_logging():
+    current_datetime = datetime.now().strftime("%Y%m%d_%H%M%S")
+    log_directory = "D:/Magisterka/Wyniki ankiet/Skany"
+    log_filename = f"log_{current_datetime}.txt"
+    log_path = os.path.join(log_directory, log_filename)
+    logging.basicConfig(filename=log_path, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def is_green(pixel, tolerance=50, target_color=(3, 254, 0)):
-    return all(abs(pixel[i] - target_color[i]) < tolerance for i in range(3))
+def is_green(pixel, tolerance=30, target_color=(3, 254, 0)):
+    return all(abs(int(pixel[i]) - target_color[i]) < tolerance for i in range(3) if isinstance(pixel[i], (int, float)))
 
 def overlap(x1, y1, w1, h1, x2, y2, w2, h2):
     return not (x1 + w1 < x2 or x2 + w2 < x1 or y1 + h1 < y2 or y2 + h2 < y1)
 
-def crop_green_frames(image_path, output_dir, max_frames=2, frame_size=(930, 930), tolerance=50, suffix="", lock=None):
-    img = Image.open(image_path)
-    width, height = img.size
+
+def clean_frames(image):
+    # Konwersja do skali szarości i czyszczenie obrazu mogą być różne w zależności od potrzeb
+    grayscale_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    _, cleaned_img = cv2.threshold(grayscale_img, 85, 255, cv2.THRESH_BINARY)
+    return cleaned_img
+
+def find_green_frames(image_path, output_dir, target_frame_count=4, frame_size=(930, 930), suffix=""):
+    img = cv2.imread(image_path)
+    hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    lower_green = np.array([36, 25, 25])
+    upper_green = np.array([86, 255, 255])
+
+    mask = cv2.inRange(hsv_img, lower_green, upper_green)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, np.ones((5, 5), np.uint8))
+
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    if len(contours) < target_frame_count:
+        logging.warning(f"Found less than {target_frame_count} contours, some frames might be missed.")
+        return
+
+    # Sortuj kontury według ich powierzchni (od największych do najmniejszych) i ogranicz do pierwszych 4
+    contours_sorted_by_area = sorted(contours, key=cv2.contourArea, reverse=True)[:target_frame_count]
+
+    # Określ pozycje konturów
+    positions = [(cv2.boundingRect(cnt)[0], cv2.boundingRect(cnt)[1]) for cnt in contours_sorted_by_area]
+
+    # Sortuj kontury na podstawie ich pozycji, aby uzyskać kolejność: lewa górna, prawa górna, lewa dolna, prawa dolna
+    contours_sorted = sorted(contours_sorted_by_area, key=lambda cnt: (cv2.boundingRect(cnt)[1], cv2.boundingRect(cnt)[0]))
+
+    # Określ środkową linię podziału pomiędzy górnymi a dolnymi ramkami
+    median_y = np.median([pos[1] for pos in positions])
+
+    # Podziel kontury na górne i dolne
+    top_contours = [cnt for cnt in contours_sorted if cv2.boundingRect(cnt)[1] < median_y]
+    bottom_contours = [cnt for cnt in contours_sorted if cv2.boundingRect(cnt)[1] >= median_y]
+
+    # Sortuj górne i dolne kontury niezależnie według osi X
+    top_sorted = sorted(top_contours, key=lambda cnt: cv2.boundingRect(cnt)[0])
+    bottom_sorted = sorted(bottom_contours, key=lambda cnt: cv2.boundingRect(cnt)[0])
+
+    # Połącz posortowane kontury w końcową kolejność
+    final_sorted_contours = top_sorted + bottom_sorted
 
     frame_count = 0
-    saved_frames = []
+    for cnt in final_sorted_contours:
+        x, y, w, h = cv2.boundingRect(cnt)
+        cropped_img = img[y:y+h, x:x+w]
+        output_path = os.path.join(output_dir, f"frame_{frame_count+1}{suffix}.jpg")
+        cv2.imwrite(output_path, clean_frames(cropped_img))
+        frame_count += 1
 
-    # Współrzędne prawej krawędzi pierwszej znalezionej ramki
-    right_edge_x = None
+    logging.info(f"Found and saved {frame_count} green frames in {image_path}")
 
-    for x in range(width):
-        for y in range(height):
-            pixel = img.getpixel((x, y))
 
-            if is_green(pixel, tolerance):
-                # Sprawdź, czy obszar o wymiarach frame_size jest w granicach obrazu
-                if x + frame_size[0] <= width and y + frame_size[1] <= height:
-                    # Jeśli nie ustalono jeszcze współrzędnej prawej krawędzi, to ustaw ją
-                    if right_edge_x is None:
-                        right_edge_x = x + frame_size[0]
+def process_single_folder(folder_path, survey_folder):
+    survey_folder_path = os.path.join(folder_path, survey_folder)
 
-                    # Wytnij obszar o rozmiarze frame_size
-                    cropped_img = img.crop((x, y, x + frame_size[0], y + frame_size[1]))
+    if os.path.isdir(survey_folder_path):
+        current_datetime = datetime.now().strftime("%Y%m%d_%H%M%S")
+        log_directory = "D:/Magisterka/Wyniki ankiet/Skany"
+        log_filename = f"log_{current_datetime}.txt"
+        log_path = os.path.join(log_directory, log_filename)
 
-                    # Sprawdź, czy nowa ramka nachodzi na już zapisane ramki
-                    overlap_flag = False
-                    for frame in saved_frames:
-                        if overlap(x, y, frame_size[0], frame_size[1], *frame):
-                            overlap_flag = True
-                            break
+        output_directory = os.path.join(survey_folder_path, f'wyniki_ankiety_{survey_folder}')
+        os.makedirs(output_directory, exist_ok=True)
 
-                    if not overlap_flag:
-                        # Zapisz wytnięty obszar
-                        frame_count += 1
-                        output_path = os.path.join(output_dir, f"frame_{frame_count}{suffix}.jpg")
-                        
-                        cleaned_img = clean_frames(cropped_img)
-                        cleaned_img.save(output_path)
+        logging.basicConfig(filename=log_path, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-                        # Zapisz współrzędne nowej ramki do listy zapisanych ramek
-                        saved_frames.append((x, y, frame_size[0], frame_size[1]))
+        logging.info(f"Przetwarzanie folderu z ankietą: {survey_folder}")
+        logging.info(f"Katalog wyjściowy: {output_directory}")
 
-                        logging.info(f"Znaleziono klatkę {frame_count} w {image_path}")
+        # Dodanie pliku tekstowego o strukturze "Label x.txt"
+        label_filename = f"Label {survey_folder}.txt"
+        label_path = os.path.join(survey_folder_path, label_filename)
 
-                        if frame_count == max_frames:
-                            break
+        with open(label_path, 'w') as label_file:
+            label_file.write("\nKLASA:  \n\n\nWiek - \nPłeć - \nMiejsce zamieszkania - \nWykształcenie - \nZatrudnienie - \n\nIlość uzyskanych punktów - ")
 
-    if frame_count == 0:
-        logging.warning(f"Brak zielonych klatek w {image_path}")
-    else:
-        logging.info(f"Znaleziono i zapisano {frame_count} zielonych klatek w {image_path}")
+        logging.info(f"Dodano plik z etykietą: {label_filename}")
+
+        image_path_a = os.path.join(survey_folder_path, f"{survey_folder}_a.jpg")
+        image_path_b = os.path.join(survey_folder_path, f"{survey_folder}_b.jpg")
+
+        if os.path.exists(image_path_a) and os.path.exists(image_path_b):
+            logging.info(f"Ścieżka obrazu A: {image_path_a}")
+            logging.info(f"Ścieżka obrazu B: {image_path_b}")
+
+            # Wykorzystaj threading.Lock() do synchronizacji wątków
+            lock = threading.Lock()
+
+            # Przekazanie lock do funkcji crop_green_frames
+            # Przekazanie lock do funkcji crop_green_frames
+            thread_a = threading.Thread(target=find_green_frames,
+                                        args=(image_path_a, output_directory, 4, (950, 950),  "_a"))
+            thread_b = threading.Thread(target=find_green_frames,
+                                        args=(image_path_b, output_directory, 4, (950, 950),  "_b"))
+            thread_a.start()
+            thread_b.start()
+
+            # Oczekiwanie na zakończenie obu wątków przed kontynuacją
+            thread_a.join()
+            thread_b.join()
+
+        elif os.path.exists(image_path_a):
+            logging.warning("Brak obrazu B dla folderu z ankietą. Pomijanie przetwarzania.")
+        elif os.path.exists(image_path_b):
+            logging.warning("Brak obrazu A dla folderu z ankietą. Pomijanie przetwarzania.")
+        else:
+            logging.error("Brak obu plików obrazów dla folderu z ankietą. Pomijanie przetwarzania.")
+
+    logging.info("Zakończono przetwarzanie wszystkich ankiet")
+    sys.exit()
 
 def process_survey_folder_parallel(folder_path):
+    setup_logging()  # Konfiguracja loggingu na początku
+    threads = []
+
     for survey_folder in os.listdir(folder_path):
         survey_folder_path = os.path.join(folder_path, survey_folder)
-
         if os.path.isdir(survey_folder_path):
-            current_datetime = datetime.now().strftime("%Y%m%d_%H%M%S")
-            log_directory = "D:/Magisterka/Wyniki ankiet/Skany"
-            log_filename = f"log_{current_datetime}.txt"
-            log_path = os.path.join(log_directory, log_filename)
+            thread = threading.Thread(target=process_single_folder, args=(folder_path, survey_folder))
+            thread.start()
+            threads.append(thread)
 
-            output_directory = os.path.join(survey_folder_path, f'wyniki_ankiety_{survey_folder}')
-            os.makedirs(output_directory, exist_ok=True)
-
-            logging.basicConfig(filename=log_path, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
-            logging.info(f"Przetwarzanie folderu z ankietą: {survey_folder}")
-            logging.info(f"Katalog wyjściowy: {output_directory}")
-
-            # Dodanie pliku tekstowego o strukturze "Label x.txt"
-            label_filename = f"Label {survey_folder}.txt"
-            label_path = os.path.join(survey_folder_path, label_filename)
-
-            with open(label_path, 'w') as label_file:
-                label_file.write("Ankieta 1\nWiek - \nPłeć - \nMiejsce zamieszkania - \nWykształcenie - ")
-
-            logging.info(f"Dodano plik z etykietą: {label_filename}")
-
-            image_path_a = os.path.join(survey_folder_path, f"{survey_folder}a.jpg")
-            image_path_b = os.path.join(survey_folder_path, f"{survey_folder}b.jpg")
-
-            if os.path.exists(image_path_a) and os.path.exists(image_path_b):
-                logging.info(f"Ścieżka obrazu A: {image_path_a}")
-                logging.info(f"Ścieżka obrazu B: {image_path_b}")
-
-                # Wykorzystaj threading.Lock() do synchronizacji wątków
-                lock = threading.Lock()
-
-                # Przekazanie lock do funkcji crop_green_frames
-                # Przekazanie lock do funkcji crop_green_frames
-                thread_a = threading.Thread(target=crop_green_frames,
-                                            args=(image_path_a, output_directory, 2, (960, 960), 50, "_a", lock))
-                thread_b = threading.Thread(target=crop_green_frames,
-                                            args=(image_path_b, output_directory, 2, (960, 960), 50, "_b", lock))
-                thread_a.start()
-                thread_b.start()
-
-                # Oczekiwanie na zakończenie obu wątków przed kontynuacją
-                thread_a.join()
-                thread_b.join()
-
-            elif os.path.exists(image_path_a):
-                logging.warning("Brak obrazu B dla folderu z ankietą. Pomijanie przetwarzania.")
-            elif os.path.exists(image_path_b):
-                logging.warning("Brak obrazu A dla folderu z ankietą. Pomijanie przetwarzania.")
-            else:
-                logging.error("Brak obu plików obrazów dla folderu z ankietą. Pomijanie przetwarzania.")
+    for thread in threads:
+        thread.join()
 
     logging.info("Zakończono przetwarzanie wszystkich ankiet")
     sys.exit()
--- test zielony.py
+++ test zielony.py
@@ -1,9 +1,9 @@
-from PIL import Image, ImageTk
+from PIL import Image
 import os
 import tkinter as tk
 from tkinter import messagebox
 
-def jest_zielony(pixel, prog=100):
+def jest_zielony(pixel, prog=140):
     return pixel[1] - pixel[0] > prog and pixel[1] - pixel[2] > prog
 
 def zamien_na_idealny_zielony(img):
@@ -20,23 +20,34 @@ def zamien_na_idealny_zielony(img):
     return img
 
 def process_survey_folder(folder_path):
-    for survey_folder in os.listdir(folder_path):
+    survey_folders = os.listdir(folder_path)
+    # Sortowanie folderów numerycznie na podstawie ich nazwy
+    survey_folders_sorted = sorted(survey_folders, key=lambda x: int(x.split(' ')[-1]))
+
+    for survey_folder in survey_folders_sorted:
         survey_folder_path = os.path.join(folder_path, survey_folder)
 
         if os.path.isdir(survey_folder_path):
+            print(f"Otwieranie folderu: {survey_folder_path}")  # Informacja o otwieranym folderze
             for image_filename in os.listdir(survey_folder_path):
-                if image_filename.lower().endswith(('.jpg', '.jpeg', '.png')):
+                if image_filename.lower().endswith(('.jpg', '.jpeg')):
                     image_path = os.path.join(survey_folder_path, image_filename)
+                    print(f"Przetwarzanie obrazu: {image_filename}")  # Informacja o przetwarzanym obrazie
 
                     try:
                         obraz = Image.open(image_path)
                         obraz_z_zielonym = zamien_na_idealny_zielony(obraz)
                         obraz_z_zielonym.save(image_path)
                     except Exception as e:
-                        print(f"Error processing image {image_filename}: {str(e)}")
+                        print(f"Błąd przetwarzania obrazu {image_filename}: {str(e)}")
 
-    messagebox.showinfo("Zakończono", "Przetwarzanie zakończone!")
-
-if __name__ == "__main__":
+def main():
+    root = tk.Tk()
+    root.withdraw()  # Ukrycie głównego okna Tkinter
     surveys_path = "D:/Magisterka/Wyniki ankiet/Skany"
     process_survey_folder(surveys_path)
+    messagebox.showinfo("Zakończono", "Przetwarzanie zakończone!")
+    root.destroy()  # Usunięcie okna po zakończeniu
+
+if __name__ == "__main__":
+    main()
